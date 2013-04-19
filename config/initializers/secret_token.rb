469bcc4f134d4016259e0647482cfbabcead84db
Aidan Feldman <aidan.feldman@gmail.com>
2013-04-18 23:31:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Govrn::Application.config.secret_token = 'b217e069894fa7c37c70bb9905be0d505adc783b13d3a0c9bb5ccf672030f2300a7d9971a20c72f364dc48267c1ce31e450ad2d398dd16415d95a4026da00924'
